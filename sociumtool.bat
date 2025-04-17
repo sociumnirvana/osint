e74669dcb9671a3001756ef09aa5c659987dc685
sociumnirvana <kasoevanana10@gmail.com>
2025-04-17 03:24:48 -0500

    Add files via upload

---
+++ sociumtool.bat
@@ -0,0 +1,20 @@
+@echo off
+title Socium dox
+color 8
+
+:menu
+cls
+echo DOX SERVICE
+echo by nirvana
+echo.
+
+echo 1. DOX-WEBSITE
+echo.
+set /p choice=Option:
+
+if "%choice%"=="1" echo doxbin
+
+:doxbin
+cls 
+echo osint.rocks
+echo.
